ce835356e55dfd843f75d80c49add2b31bdcf155
Mark Jacobs <mark47jacobs>
2021-03-21 12:02:37 +0530

    minor

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LandingPageComponent implements OnInit {
 
   constructor() { }
-
+  CurQuote: string;
   ngOnInit(): void {
     this.animate();
   }
