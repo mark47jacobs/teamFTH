ad68e3b1fa1922d66828fee34058ae8227d57d39
Mark Jacobs <mark47jacobs>
2021-03-21 17:12:32 +0530

    minor

---
+++ questions.js
@@ -0,0 +1,91 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Name the first woman who went in space?",
+    answer: "Valentina Tereshkova",
+    options: [
+      "Yuri Gagarin",
+      "Sunita Williams",
+      "Kalpana Chawla",
+      "Valentina Tereshkova"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Name the first woman president of India?",
+    answer: "Pratibha Patil",
+    options: [
+      "Indira Gandhi",
+      "Sarojni Naidu",
+      "Pratibha Patil",
+      "Meera Kumari"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Name the first woman to win a nobel prize?",
+    answer: "Marie Curie",
+    options: [
+      "Marie Curie",
+      "Mother Teresa",
+      "Betty Williams",
+      "Aung San Suu Kyi"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the first woman judge of supreme court?",
+    answer: "Justice Fathima Beevi",
+    options: [
+      "Justice Fathima Beevi",
+      "Justice Indira Banerjee",
+      "Justice Sujata Manohar",
+      "Justice Ranjana Desai"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Name the first Indian woman to win an Olympic Medal?",
+    answer: "Karnam Malleswari",
+    options: [
+      "P.T Usha",
+      "Mary Kom",
+      "P.V Sindhu",
+      "Karnam Malleswari"
+    ]
+  },
+  {
+  numb: 6,
+  question: "Name the first woman to climb the Mount Everest?",
+  answer: "Junko Tabei",
+  options: [
+    "Bachendri Pal",
+    "Santosh Yadav",
+    "Junko Tabei",
+    "Arunima Sinha"
+  ]
+},
+{
+numb: 7,
+question: "Name the first Indian woman to win the Miss Universe Title?",
+answer: "Sushmita Sen",
+options: [
+  "Priyanka Chopra",
+  "Dia Mirza",
+  "Aishwarya Rai",
+  "Sushmita Sen"
+]
+},
+{
+numb: 8,
+question: "Name the first woman computer Programmer?",
+answer: "Lady Ada Lovelace",
+options: [
+  "Grace Hopper",
+  "Katherine Johnson",
+  "Lady Ada Lovelace",
+  "Megan Smith"
+]
+},
+];
