89786303c8f4d8a965ef441fc6b8ca33c7fb9d7f
Mark Jacobs <mark47jacobs>
2021-03-21 16:46:52 +0530

    minor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { QuizComponent } from './quiz/quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'quiz', component: QuizComponent },
+  { path: 'landing', component: LandingPageComponent },
+  { path: "", redirectTo: "landing", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
